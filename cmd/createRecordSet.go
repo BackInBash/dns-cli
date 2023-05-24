41811d9da162777bc9217a5fb642ef69a9c39472
BackInBash <48181660+BackInBash@users.noreply.github.com>
2023-05-24 09:25:48 +0200

    Update createRecordSet.go

--- cmd/createRecordSet.go
+++ cmd/createRecordSet.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"text/tabwriter"
 
 	"github.com/BackInBash/dns-cli/internal/api"
 	"github.com/spf13/cobra"
@@ -14,10 +16,11 @@ var (
 	createRecordName string
 	createRecordType string
 	createRecords    string
+	createZoneId     string
 )
 
 var createRecordCmd = &cobra.Command{
-	Use:   "redord",
+	Use:   "record",
 	Short: "Creates a new DNS Record.",
 	Long:  `Creates a new DNS Record.`,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -33,8 +36,14 @@ func init() {
 	createRecordCmd.PersistentFlags().StringVar(&createRecordName, "name", "", "The name to set for the instance.")
 	_ = createRecordCmd.MarkPersistentFlagRequired("name")
 
-	createZoneCmd.PersistentFlags().StringVar(&createRecordType, "dns-name", "", "The Domain DNS Name.")
-	_ = createZoneCmd.MarkPersistentFlagRequired("dns-name")
+	createRecordCmd.PersistentFlags().StringVar(&createRecordType, "type", "", "The DNS Record Type.")
+	_ = createRecordCmd.MarkPersistentFlagRequired("type")
+
+	createRecordCmd.PersistentFlags().StringVar(&createZoneId, "zone-id", "", "The DNS Zone ID.")
+	_ = createRecordCmd.MarkPersistentFlagRequired("zone-id")
+
+	createRecordCmd.PersistentFlags().StringVar(&createRecords, "records", "", "The DNS Record IP.")
+	_ = createRecordCmd.MarkPersistentFlagRequired("records")
 }
 
 func createRecord() error {
@@ -42,14 +51,20 @@ func createRecord() error {
 	if err != nil {
 		return err
 	}
-	request := api.PostV1ProjectsProjectIdZonesJSONRequestBody{
-		// DnsName zone name
-		DnsName: createDNSZoneName,
 
+	request := api.PostV1ProjectsProjectIdZonesZoneIdRrsetsJSONRequestBody{
 		// Name user given name
-		Name: createZoneName,
+		Name: createRecordName,
+
+		// DNS Record IP
+		Records: []api.RrsetRecordPost{{Content: createRecords}},
+
+		// DNS Type
+		Type: api.RrsetRRSetPostType(createRecordType),
 	}
-	response, err := client.PostV1ProjectsProjectIdZones(context.Background(), projectId, request)
+	response, err := client.PostV1ProjectsProjectIdZonesZoneIdRrsets(context.Background(), projectId, createZoneId, request)
+	enc := json.NewEncoder(os.Stdout)
+	enc.Encode(request)
 	if err != nil {
 		return fmt.Errorf("failed to create instance: %w", err)
 	}
@@ -57,12 +72,26 @@ func createRecord() error {
 		return fmt.Errorf("unexpected status code: %s", response.Status)
 	}
 
-	var instance api.ZoneResponseZone
+	var instance api.RrsetResponseRRSet
 	if err := json.NewDecoder(response.Body).Decode(&instance); err != nil {
 		return fmt.Errorf("failed to decode response: %w", err)
 	}
-	if err := printZone(instance.Zone); err != nil {
+	if err := printRset(instance.Rrset); err != nil {
 		return err
+	}
+	return nil
+}
+
+func printRset(rset api.DomainRRSet) error {
+	writer := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+	defer writer.Flush()
+	_, err := fmt.Fprintf(writer, "Id\tName\tRecords\tType\tActive\n")
+	if err != nil {
+		return fmt.Errorf("failed to write to tabwriter: %w", err)
+	}
+	_, err = fmt.Fprintf(writer, "%s\t%s\t%s\t%s\t%t\n", rset.Id, rset.Name, rset.Records, rset.Type, rset.Active)
+	if err != nil {
+		return fmt.Errorf("failed to write to tabwriter: %w", err)
 	}
 	return nil
 }
